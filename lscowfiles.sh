01cc886c6c2576328b3284a8f0ffb94b97e642c7
stardiviner <numbchild@gmail.com>
2012-10-07 10:34:46 +0800

    ubuntu has different cows location.

--- lscowfiles.sh
+++ lscowfiles.sh
@@ -1,4 +1,3 @@
-
 #
 # Just to see them all (not loaded by vim)
 #
@@ -7,7 +6,13 @@
 # There are some funny cowfiles in there...
 #
 
-files=$(ls /usr/local/share/cows)
+if [ -d /usr/local/share/cows ]; then
+  files=$(ls /usr/local/share/cows)
+elif [ -d /usr/share/cowsay/cows/ ]; then
+  files=$(/usr/share/cowsay/cows/)
+fi
+
+
 for cowfile in $files; do
   echo "Cowfile $cowfile"
   echo """
